9155f032f167c1bfed9e96485b4f752e5e3d6c7b
jeann <jeannguilherme@hotmail.com>
2023-05-25 11:06:23 -0300

    subindo tudo pra develop

--- src/routes/Pokedex.tsx
+++ src/routes/Pokedex.tsx
@@ -1,135 +1,140 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-
-import Footer from '../components/footer/footer';
-
+import { Card, Container, Grid } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import PokemonCard from '../components/cards/card'
+import axios from 'axios'
+import Footer from '../components/footer/footer'
 import styles from './index.module.css'
-
-interface Pokemon {
-  [x: string]: any;
-  name: string;
-  weight: number;
-  sprites: {
-    front_default: string;
-  };
-  stats: {
-    base_stat: number;
-    stat: {
-      name: string;
-    };
-  }[];
-  abilities: {
-    ability: {
-      name: string;
-    };
-  }[];
-  types: {
-    type: {
-      name: string;
-    };
-  }[];
-}
-
-interface Species {
-  is_legendary: boolean;
-}
+import { Pokemon } from '../components/interface/interface'
+import { Species  } from '../components/interface/interfaceLendario'
 
 const Pokedex = () => {
-  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
-  const [searchTerm, setSearchTerm] = useState('');
-  const [foundPokemon, setFoundPokemon] = useState<Pokemon | null>(null);
+    useEffect(() => {
+        getPokemons()
+    })
 
-  const getPokemons = async () => {
-    try {
-      const response = await axios.get('https://pokeapi.co/api/v2/pokemon/?limit=100');
-      const data = response.data.results;
-      setPokemon(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+    const [pokemons, setPokemons] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [foundPokemon, setFoundPokemon] = useState<Pokemon | null>(null);
 
-  useEffect(() => {
-    getPokemons();
-  }, []);
+    //MOSTRA TODOS OS POKEMONS EM TELA
+    const getPokemons = () => {
+        var endpoints = [];
+        for(var i = 1 ; i<100 ; i++){
+            endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
+        }
+        let response = axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then((res) => setPokemons(res));
+    };
 
-  const getPokemonDetails = async (name: string) => {
-    try {
-      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
-      const data = response.data;
-      const speciesResponse = await axios.get(data.species.url);
-      const speciesData: Species = speciesResponse.data;
-      const isLegendary = speciesData.is_legendary;
-      setFoundPokemon({ ...data, isLegendary });
-    } catch (error) {
-      console.log(error);
-      setFoundPokemon(null);
-    }
-  };
+    const getPokemonDetails = async (name: string) => {
+      try {
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+        const data = response.data;
+        const speciesResponse = await axios.get(data.species.url);
+        const speciesData: Species = speciesResponse.data;
+        const isLegendary = speciesData.is_legendary;
+        const typeClassName = data.types[0].type.name;
+        setFoundPokemon({ ...data, isLegendary });
+      } catch (error) {
+        console.log(error);
+        setFoundPokemon(null);
+      }
+    };
+  
+    const calculateHP = (stats: { base_stat: number; stat: { name: string } }[]) => {
+      const hpStat = stats.find((stat) => stat.stat.name === 'hp');
+      if (hpStat) {
+        return hpStat.base_stat;
+      }
+      return 0;
+    };
+  
+    const handleSearch = async () => {
+      try {
+        await getPokemonDetails(searchTerm.toLowerCase());
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  
+    const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+      if (event.key === 'Enter') {
+        handleSearch();
+      }
+    };
+  
 
-  const calculateHP = (stats: { base_stat: number; stat: { name: string } }[]) => {
-    const hpStat = stats.find((stat) => stat.stat.name === 'hp');
-    if (hpStat) {
-      return hpStat.base_stat;
-    }
-    return 0;
-  };
+    return (
+        <>
+          <div className="container">
+            <div className="row">
+              <div className="col py-4">
+                <h1>
+                  800 <span>Pokemons</span> for you to choose your favorite
+                </h1>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col">
+              <input  
+                className="form-control" 
+                name="search" 
+                maxLength={25}
+                type="text" 
+                placeholder="Find your Pokemon..."
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                onKeyPress={handleKeyPress}
+                />
+              </div>
+            </div>
 
-  const handleSearch = async () => {
-    try {
-      await getPokemonDetails(searchTerm.toLowerCase());
-    } catch (error) {
-      console.log(error);
-    }
-  };
+            {foundPokemon ? (
+              
+              
+              <div className={styles.modal_poke}>
+                <div className={styles.modal_dialog}>
+                  <div className={styles.modal_content}>
 
-  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === 'Enter') {
-      handleSearch();
-    }
-  };
+                    <div className='row'>
+                    <div className={`col-4 bg-${foundPokemon.typeClassName}`}>
+                    <img className='img-fluid' src={foundPokemon.sprites.other['official-artwork'].front_shiny } alt={foundPokemon.name} />
+                    </div>
+                    <div className='col-8'>
+                    <h2>{foundPokemon.name}</h2>
+                    <p>Weight: {foundPokemon.weight}</p>
+                    <p>HP: {calculateHP(foundPokemon.stats)}</p>
+                    <p>Attack: {foundPokemon.stats.find((stat) => stat.stat.name === 'attack')?.base_stat}</p>
+                    <p>Defense: {foundPokemon.stats.find((stat) => stat.stat.name === 'defense')?.base_stat}</p>
+                    <p>Sp. Attack: {foundPokemon.stats.find((stat) => stat.stat.name === 'special-attack')?.base_stat}</p>
+                    <p>Sp. Defense: {foundPokemon.stats.find((stat) => stat.stat.name === 'special-defense')?.base_stat}</p>
+                    <p>Is Legendary: {foundPokemon.isLegendary ? 'Yes' : 'No'}</p>
+                    <p>Type: {foundPokemon.types.map((type) => type.type.name).join(', ')}</p>
+                    <p>Abilities: {foundPokemon.abilities.map((ability) => ability.ability.name).join(', ')}</p>
+                    </div>
+                    </div>
 
-  return (
-    <div>
-      <div>
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          onKeyPress={handleKeyPress}
-          placeholder="Search Pokemon"
-        />
-      </div>
-      {foundPokemon ? (
-        <div>
-          <h2>{foundPokemon.name}</h2>
-          <p>Weight: {foundPokemon.weight}</p>
-          <p>HP: {calculateHP(foundPokemon.stats)}</p>
-          <p>Attack: {foundPokemon.stats.find((stat) => stat.stat.name === 'attack')?.base_stat}</p>
-          <p>Defense: {foundPokemon.stats.find((stat) => stat.stat.name === 'defense')?.base_stat}</p>
-          <p>Sp. Attack: {foundPokemon.stats.find((stat) => stat.stat.name === 'special-attack')?.base_stat}</p>
-          <p>Sp. Defense: {foundPokemon.stats.find((stat) => stat.stat.name === 'special-defense')?.base_stat}</p>
-          <p>Is Legendary: {foundPokemon.isLegendary ? 'Yes' : 'No'}</p>
-          <p>Type: {foundPokemon.types.map((type) => type.type.name).join(', ')}</p>
-          <p>Abilities: {foundPokemon.abilities.map((ability) => ability.ability.name).join(', ')}</p>
-          
-          <img src={foundPokemon.sprites.front_default} alt={foundPokemon.name} />
-        </div>
-      ) : (
-        <div>
-          {pokemon.map((poke) => (
-            <Link key={poke.name} to={`/pokemon/${poke.name}`}>
-              {poke.name}
-            </Link>
-          ))}
-        </div>
-      )}
-      <div className={`container-fluid g-0 ${styles.div_footer}`}>
-            <Footer/>
-      </div>
-    </div>
-  );
-};
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div>
+             
+              </div>
+            )}
 
-export default Pokedex;
+
+
+            <div className="row py-5">
+            {pokemons.map((pokemon, key) => 
+              <div className="col-sm-12 col-md-6 col-lg-4 col-xl-4" key={key}>
+                  <PokemonCard name={pokemon.data.name} image={pokemon.data.sprites.other['official-artwork'].front_shiny } types={pokemon.data.types} stats={pokemon.data.stats} typeClass={pokemon.data.types}/>
+              </div> 
+            )}
+            </div>
+          </div>
+        </>
+    )
+}
+
+export default Pokedex
